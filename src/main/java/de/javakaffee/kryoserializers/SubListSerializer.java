6192f3976ac7fe5f7d09a1dc000eab663c773461
Martin Grotzke <martin.grotzke@javakaffee.de>
2012-08-08 21:53:23 +0200

    Improve SubListSerializer to support java7 ArrayList.subList
    In java7 ArrayList.subList returns a java.util.ArrayList$SubList (and
    not a java.util.SubList as in java6 or as AbstractList.subList does it)

--- src/main/java/de/javakaffee/kryoserializers/SubListSerializer.java
+++ src/main/java/de/javakaffee/kryoserializers/SubListSerializer.java
@@ -32,33 +32,146 @@ import com.esotericsoftware.kryo.serialize.IntSerializer;
 @SuppressWarnings( "unchecked" )
 public class SubListSerializer extends Serializer {
     
-    private static final Class<?> SUBLIST_CLASS = getClass( "java.util.SubList" );
+    static class JavaUtilSubListSerializer extends Serializer {
 
-    private final Kryo _kryo;
-    private Field _listField;
-    private Field _offsetField;
-    private Field _sizeField;
+        private static final Class<?> SUBLIST_CLASS = SubListSerializer.getClass( "java.util.SubList" );
 
-    public SubListSerializer( final Kryo kryo ) {
-        _kryo = kryo;
-        try {
-            final Class<?> clazz = Class.forName( "java.util.SubList" );
-            _listField = clazz.getDeclaredField( "l" );
-            _offsetField = clazz.getDeclaredField( "offset" );
-            _sizeField = clazz.getDeclaredField( "size" );
-            _listField.setAccessible( true );
-            _offsetField.setAccessible( true );
-            _sizeField.setAccessible( true );
-        } catch ( final Exception e ) {
-            throw new RuntimeException( e );
+        private final Kryo _kryo;
+        private Field _listField;
+        private Field _offsetField;
+        private Field _sizeField;
+
+        public JavaUtilSubListSerializer( final Kryo kryo ) {
+            _kryo = kryo;
+            try {
+                final Class<?> clazz = SUBLIST_CLASS;
+                _listField = clazz.getDeclaredField( "l" );
+                _offsetField = clazz.getDeclaredField( "offset" );
+                _sizeField = clazz.getDeclaredField( "size" );
+                _listField.setAccessible( true );
+                _offsetField.setAccessible( true );
+                _sizeField.setAccessible( true );
+            } catch ( final Exception e ) {
+                throw new RuntimeException( e );
+            }
+        }
+
+        static boolean canSerialize( final Class<?> type ) {
+            return SUBLIST_CLASS != null && SUBLIST_CLASS.isAssignableFrom( type );
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public <T> T readObjectData( final ByteBuffer buffer, final Class<T> clazz ) {
+            final List<?> list = (List<?>) _kryo.readClassAndObject( buffer );
+            final int fromIndex = IntSerializer.get( buffer, true );
+            final int toIndex = IntSerializer.get( buffer, true );
+            return (T) list.subList( fromIndex, toIndex );
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void writeObjectData( final ByteBuffer buffer, final Object obj ) {
+            try {
+                _kryo.writeClassAndObject( buffer, _listField.get( obj ) );
+                final int fromIndex = _offsetField.getInt( obj );
+                IntSerializer.put( buffer, fromIndex, true );
+                final int toIndex = fromIndex + _sizeField.getInt( obj );
+                IntSerializer.put( buffer, toIndex, true );
+            } catch ( final RuntimeException e ) {
+                // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
+                // handles SerializationException specifically (resizing the buffer)...
+                throw e;
+            } catch ( final Exception e ) {
+                throw new RuntimeException( e );
+            }
+        }
+    }
+
+    static class ArrayListSubListSerializer extends Serializer {
+
+        private static final Class<?> SUBLIST_CLASS = SubListSerializer.getClass("java.util.ArrayList$SubList");
+
+        private final Kryo _kryo;
+        private Field _parentField;
+        private Field _parentOffsetField;
+        private Field _sizeField;
+
+        public ArrayListSubListSerializer( final Kryo kryo ) {
+            _kryo = kryo;
+            try {
+                final Class<?> clazz = Class.forName( "java.util.ArrayList$SubList" );
+                _parentField = clazz.getDeclaredField( "parent" );
+                _parentOffsetField = clazz.getDeclaredField( "parentOffset" );
+                _sizeField = clazz.getDeclaredField( "size" );
+                _parentField.setAccessible( true );
+                _parentOffsetField.setAccessible( true );
+                _sizeField.setAccessible( true );
+            } catch ( final Exception e ) {
+                throw new RuntimeException( e );
+            }
+        }
+
+        static boolean canSerialize( final Class<?> type ) {
+            return SUBLIST_CLASS != null && SUBLIST_CLASS.isAssignableFrom( type );
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public <T> T readObjectData( final ByteBuffer buffer, final Class<T> clazz ) {
+            final List<?> list = (List<?>) _kryo.readClassAndObject( buffer );
+            final int fromIndex = IntSerializer.get( buffer, true );
+            final int toIndex = IntSerializer.get( buffer, true );
+            return (T) list.subList( fromIndex, toIndex );
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void writeObjectData( final ByteBuffer buffer, final Object obj ) {
+            try {
+                _kryo.writeClassAndObject( buffer, _parentField.get( obj ) );
+                final int parentOffset = _parentOffsetField.getInt( obj );
+                final int fromIndex = parentOffset;
+                IntSerializer.put( buffer, fromIndex, true );
+                final int toIndex = fromIndex + _sizeField.getInt( obj );
+                IntSerializer.put( buffer, toIndex, true );
+            } catch ( final RuntimeException e ) {
+                // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
+                // handles SerializationException specifically (resizing the buffer)...
+                throw e;
+            } catch ( final Exception e ) {
+                throw new RuntimeException( e );
+            }
+        }
+    }
+
+    private Serializer _serializer;
+
+    public SubListSerializer( final Kryo kryo, final Class<? extends List<?>> type ) {
+        if(JavaUtilSubListSerializer.canSerialize(type)) {
+            _serializer = new JavaUtilSubListSerializer(kryo);
+        }
+        else if (ArrayListSubListSerializer.canSerialize(type)) {
+            _serializer = new ArrayListSubListSerializer(kryo);
+        }
+        else {
+            throw new IllegalArgumentException("Unsupported type " + type + ". Check canSerialize(Class) to see if this serializer can handle a given type.");
         }
     }
     
     private static Class<?> getClass( final String className ) {
         try {
             return Class.forName( className );
-        } catch ( final Exception e ) {
-            throw new RuntimeException( e );
+        } catch ( final ClassNotFoundException e ) {
+            return null;
         }
     }
 
@@ -68,7 +181,7 @@ public class SubListSerializer extends Serializer {
      * @return <code>true</code> if the given class can be serialized/deserialized by this serializer.
      */
     public static boolean canSerialize( final Class<?> type ) {
-        return SUBLIST_CLASS.isAssignableFrom( type );
+        return JavaUtilSubListSerializer.canSerialize(type) || ArrayListSubListSerializer.canSerialize(type);
     }
 
     /**
@@ -76,10 +189,7 @@ public class SubListSerializer extends Serializer {
      */
     @Override
     public <T> T readObjectData( final ByteBuffer buffer, final Class<T> clazz ) {
-        final List<?> list = (List<?>) _kryo.readClassAndObject( buffer );
-        final int fromIndex = IntSerializer.get( buffer, true );
-        final int toIndex = IntSerializer.get( buffer, true );
-        return (T) list.subList( fromIndex, toIndex );
+        return _serializer.readObjectData(buffer, clazz);
     }
 
     /**
@@ -87,19 +197,7 @@ public class SubListSerializer extends Serializer {
      */
     @Override
     public void writeObjectData( final ByteBuffer buffer, final Object obj ) {
-        try {
-            _kryo.writeClassAndObject( buffer, _listField.get( obj ) );
-            final int fromIndex = _offsetField.getInt( obj );
-            IntSerializer.put( buffer, fromIndex, true );
-            final int toIndex = fromIndex + _sizeField.getInt( obj );
-            IntSerializer.put( buffer, toIndex, true );
-        } catch ( final RuntimeException e ) {
-            // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
-            // handles SerializationException specifically (resizing the buffer)...
-            throw e;
-        } catch ( final Exception e ) {
-            throw new RuntimeException( e );
-        }
+        _serializer.writeObjectData(buffer, obj);
     }
 
 }
--- src/test/java/de/javakaffee/kryoserializers/SubListSerializerTest.java
+++ src/test/java/de/javakaffee/kryoserializers/SubListSerializerTest.java
@@ -20,8 +20,10 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.esotericsoftware.kryo.Kryo;
@@ -35,29 +37,55 @@ import com.esotericsoftware.kryo.Serializer;
  */
 public class SubListSerializerTest {
 
-    @Test( enabled = true )
-    public void testFieldSerializer () throws Exception {
-        final Kryo kryo = new KryoReflectionFactorySupport() {
+    private Kryo _kryo;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        _kryo = new KryoReflectionFactorySupport() {
             
             @Override
-            @SuppressWarnings( "unchecked" )
+            @SuppressWarnings({ "rawtypes", "unchecked" })
             public Serializer newSerializer( final Class type ) {
                 if ( SubListSerializer.canSerialize( type ) ) {
-                    return new SubListSerializer( this );
+                    return new SubListSerializer( this, type );
                 }
                 return super.newSerializer( type );
             }
         };
-        kryo.setRegistrationOptional( true );
-        
-        final List<TestEnum> subList = new ArrayList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 2 );
-        final byte[] serialized = new ObjectBuffer( kryo, 1024 * 1024 ).writeObject( subList );
+        _kryo.setRegistrationOptional( true );
+    }
+
+    @Test( enabled = true )
+    public void testSubList () throws Exception {
+        final List<TestEnum> subList = new LinkedList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 2 );
+        doTest(subList);
+    }
+
+    private void doTest(final List<TestEnum> subList) {
+        final byte[] serialized = new ObjectBuffer( _kryo, 1024 * 1024 ).writeObject( subList );
         @SuppressWarnings( "unchecked" )
-        final List<TestEnum> deserialized = new ObjectBuffer( kryo, 1024 * 1024 ).readObject( serialized, subList.getClass() );
+        final List<TestEnum> deserialized = new ObjectBuffer( _kryo, 1024 * 1024 ).readObject( serialized, subList.getClass() );
 
         assertEquals( deserialized, subList );
         assertEquals( deserialized.remove( 0 ), subList.remove( 0 ) );
-        
+    }
+
+    @Test( enabled = true )
+    public void testSubListSubList () throws Exception {
+        final List<TestEnum> subList = new LinkedList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 3 ).subList(1, 2);
+        doTest(subList);
+    }
+
+    @Test( enabled = true )
+    public void testArrayListSubList () throws Exception {
+        final List<TestEnum> subList = new ArrayList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 2 );
+        doTest(subList);
+    }
+
+    @Test( enabled = true )
+    public void testArrayListSubListSubList () throws Exception {
+        final List<TestEnum> subList = new ArrayList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 3 ).subList(1, 2);
+        doTest(subList);
     }
     
     static enum TestEnum {
