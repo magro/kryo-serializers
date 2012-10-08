e8642ff80ab2cc7fc14d2714ebe19d97eee444cd
Martin Grotzke <martin.grotzke@javakaffee.de>
2012-10-08 23:41:10 +0200

    Improve SubListSerializer to support java7

---
+++ src/main/java/de/javakaffee/kryoserializers/SubListSerializers.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2010 Martin Grotzke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package de.javakaffee.kryoserializers;
+
+import java.lang.reflect.Field;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+/**
+ * Kryo {@link Serializer}s for lists created via {@link List#subList(int, int)}.
+ * An instance of a serializer can be obtained via {@link #createFor(Class)}, which
+ * just returns <code>null</code> if the given type is not supported by these
+ * serializers.
+ * 
+ * @author <a href="mailto:martin.grotzke@javakaffee.de">Martin Grotzke</a>
+ */
+public class SubListSerializers {
+
+    static Class<?> getClass(final String className) {
+        try {
+            return Class.forName(className);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Obtain a serializer for the given sublist type. If the type is not supported
+     * <code>null</code> is returned.
+     * @param type the class of the sublist.
+     * @return a serializer instance or <code>null</code>.
+     */
+    @SuppressWarnings("rawtypes")
+    public static Serializer<List<?>> createFor(final Class type) {
+        if (ArrayListSubListSerializer.canSerialize(type))
+            return new ArrayListSubListSerializer();
+        if (JavaUtilSubListSerializer.canSerialize(type))
+            return new JavaUtilSubListSerializer();
+        return null;
+    }
+
+    /**
+     * Supports sublists created via {@link ArrayList#subList(int, int)} since java7 (oracle jdk,
+     * represented by <code>java.util.ArrayList$SubList</code>).
+     */
+    public static class ArrayListSubListSerializer extends Serializer<List<?>> {
+
+        private static final Class<?> SUBLIST_CLASS = SubListSerializers.getClass("java.util.ArrayList$SubList");
+
+        private Field _parentField;
+        private Field _parentOffsetField;
+        private Field _sizeField;
+
+        public ArrayListSubListSerializer() {
+            try {
+                final Class<?> clazz = Class.forName("java.util.ArrayList$SubList");
+                _parentField = clazz.getDeclaredField("parent");
+                _parentOffsetField = clazz.getDeclaredField( "parentOffset" );
+                _sizeField = clazz.getDeclaredField( "size" );
+                _parentField.setAccessible( true );
+                _parentOffsetField.setAccessible( true );
+                _sizeField.setAccessible( true );
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        /**
+         * Can be used to determine, if the given type can be handled by this serializer.
+         * 
+         * @param type
+         *            the class to check.
+         * @return <code>true</code> if the given class can be serialized/deserialized by this serializer.
+         */
+        public static boolean canSerialize(final Class<?> type) {
+            return SUBLIST_CLASS.isAssignableFrom(type);
+        }
+
+        @Override
+        public List<?> read(final Kryo kryo, final Input input, final Class<List<?>> clazz) {
+            final List<?> list = (List<?>) kryo.readClassAndObject(input);
+            final int fromIndex = input.readInt(true);
+            final int toIndex = input.readInt(true);
+            return list.subList(fromIndex, toIndex);
+        }
+
+        @Override
+        public void write(final Kryo kryo, final Output output, final List<?> obj) {
+            try {
+                kryo.writeClassAndObject(output, _parentField.get(obj));
+                final int parentOffset = _parentOffsetField.getInt( obj );
+                final int fromIndex = parentOffset;
+                output.writeInt(fromIndex, true);
+                final int toIndex = fromIndex + _sizeField.getInt( obj );
+                output.writeInt(toIndex, true);
+            } catch (final RuntimeException e) {
+                // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
+                // handles SerializationException specifically (resizing the buffer)...
+                throw e;
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Supports sublists created via {@link AbstractList#subList(int, int)}, e.g. LinkedList.
+     * In oracle jdk such sublists are represented by <code>java.util.SubList</code>.
+     */
+    public static class JavaUtilSubListSerializer extends Serializer<List<?>> {
+
+        private static final Class<?> SUBLIST_CLASS = SubListSerializers.getClass("java.util.SubList");
+
+        private Field _listField;
+        private Field _offsetField;
+        private Field _sizeField;
+
+        public JavaUtilSubListSerializer() {
+            try {
+                final Class<?> clazz = Class.forName("java.util.SubList");
+                _listField = clazz.getDeclaredField("l");
+                _offsetField = clazz.getDeclaredField("offset");
+                _sizeField = clazz.getDeclaredField("size");
+                _listField.setAccessible(true);
+                _offsetField.setAccessible(true);
+                _sizeField.setAccessible(true);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        /**
+         * Can be used to determine, if the given type can be handled by this serializer.
+         * 
+         * @param type
+         *            the class to check.
+         * @return <code>true</code> if the given class can be serialized/deserialized by this serializer.
+         */
+        public static boolean canSerialize(final Class<?> type) {
+            return SUBLIST_CLASS.isAssignableFrom(type);
+        }
+
+        @Override
+        public List<?> read(final Kryo kryo, final Input input, final Class<List<?>> clazz) {
+            final List<?> list = (List<?>) kryo.readClassAndObject(input);
+            final int fromIndex = input.readInt(true);
+            final int toIndex = input.readInt(true);
+            return list.subList(fromIndex, toIndex);
+        }
+
+        @Override
+        public void write(final Kryo kryo, final Output output, final List<?> obj) {
+            try {
+                kryo.writeClassAndObject(output, _listField.get(obj));
+                final int fromIndex = _offsetField.getInt(obj);
+                output.writeInt(fromIndex, true);
+                final int toIndex = fromIndex + _sizeField.getInt(obj);
+                output.writeInt(toIndex, true);
+            } catch (final RuntimeException e) {
+                // Don't eat and wrap RuntimeExceptions because the ObjectBuffer.write...
+                // handles SerializationException specifically (resizing the buffer)...
+                throw e;
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ src/test/java/de/javakaffee/kryoserializers/SubListSerializersTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010 Martin Grotzke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package de.javakaffee.kryoserializers;
+
+import static de.javakaffee.kryoserializers.KryoTest.deserialize;
+import static de.javakaffee.kryoserializers.KryoTest.serialize;
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+
+/**
+ * Test for {@link SubListSerializers}.
+ *
+ * @author <a href="mailto:martin.grotzke@javakaffee.de">Martin Grotzke</a>
+ */
+public class SubListSerializersTest {
+    
+    private Kryo _kryo;
+
+    @BeforeClass
+    public void beforeClass() {
+        _kryo = new KryoReflectionFactorySupport() {
+            
+            @Override
+            @SuppressWarnings("rawtypes")
+            public Serializer newSerializer(final Class<? extends Serializer> serializerClass, final Class type) {
+                final Serializer<List<?>> subListSerializer = SubListSerializers.createFor(type);
+                if ( subListSerializer != null ) {
+                    return subListSerializer;
+                }
+                return super.newSerializer( serializerClass, type );
+            }
+        };
+    }
+
+    private void doTest(final List<TestEnum> subList) {
+        final byte[] serialized = serialize( _kryo, subList );
+        @SuppressWarnings( "unchecked" )
+        final List<TestEnum> deserialized = deserialize( _kryo, serialized, subList.getClass() );
+
+        assertEquals( deserialized, subList );
+        assertEquals( deserialized.remove( 0 ), subList.remove( 0 ) );
+    }
+
+    @Test( enabled = true )
+    public void testSubList () throws Exception {
+        final List<TestEnum> subList = new LinkedList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 2 );
+        doTest(subList);
+    }
+
+    @Test( enabled = true )
+    public void testSubListSubList () throws Exception {
+        final List<TestEnum> subList = new LinkedList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 3 ).subList(1, 2);
+        doTest(subList);
+    }
+
+    @Test( enabled = true )
+    public void testArrayListSubList () throws Exception {
+        final List<TestEnum> subList = new ArrayList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 2 );
+        doTest(subList);
+    }
+
+    @Test( enabled = true )
+    public void testArrayListSubListSubList () throws Exception {
+        final List<TestEnum> subList = new ArrayList<TestEnum>( Arrays.asList( TestEnum.values() ) ).subList( 1, 3 ).subList(1, 2);
+        doTest(subList);
+    }
+    
+    static enum TestEnum {
+        ITEM1, ITEM2, ITEM3;
+    }
+
+}
