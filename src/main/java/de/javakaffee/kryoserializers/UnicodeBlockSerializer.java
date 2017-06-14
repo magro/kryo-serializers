c4ce10fb23ee6f7dc63991d4cffb569370dcb6ea
Pr0methean <seahen123@gmail.com>
2017-06-13 23:24:29 -0700

    UnicodeBlockSerializer (#70)
    * UnicodeBlockSerializer
    
    New serializer for Character.UnicodeBlock, with test. Takes into account
    that this class is effectively an enum.
    
    * Explicit type arguments
    
    Remove use of diamond operator, to restore JDK6 compatibility
    
    * Fix syntax error
    
    * Fix syntax error
    
    * Fix syntax error
    
    * Fix syntax error
    
    * Fix test issues
    
    * Strengthen assertEquals to assertSame
    
    * Fix test issue
    
    * Fix test issue; switch to more conventional var name
    
    * Add copy method
    
    * Temporary debugging code
    
    * Fix NPE from deserializing unnamed block
    
    * More debugging code
    
    * Switch to Logger, since Travis doesn't give stdout
    
    * Switch to getDeclaredFields()
    
    getFields() isn't working
    
    * Dump missing fields
    
    * Set fields accessible if needed
    
    * Remove debugging code; finish Javadoc comments
    
    * Remove debugging code
    
    * Add UnicodeBlockSerializer to README

---
+++ src/main/java/de/javakaffee/kryoserializers/UnicodeBlockSerializer.java
@@ -0,0 +1,72 @@
+package de.javakaffee.kryoserializers;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.lang.Character.UnicodeBlock;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.IdentityHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A kryo {@link Serializer} for fields of type {@link UnicodeBlock}, which is effectively but not
+ * actually an enum.
+ *
+ * @author <a href="mailto:seahen123@gmail.com">Chris Hennick</a>
+ */
+public class UnicodeBlockSerializer extends Serializer<UnicodeBlock> {
+    private static final IdentityHashMap<UnicodeBlock, String> BLOCK_NAMES
+            = new IdentityHashMap<UnicodeBlock, String>();
+    static {
+        // Reflectively look up the instances and their names, which are in UnicodeBlock's static
+        // fields (necessary since UnicodeBlock isn't an actual enum)
+        for (Field field : UnicodeBlock.class.getDeclaredFields()) {
+            if (Modifier.isStatic(field.getModifiers())) {
+                try {
+                    // For some reason, UnicodeBlock constants aren't already accessible, even
+                    // though they're public! WTF?
+                    field.setAccessible(true);
+                    Object value = field.get(null);
+                    if (value instanceof UnicodeBlock) {
+                        BLOCK_NAMES.put((UnicodeBlock) value, field.getName());
+                    }
+                } catch (IllegalAccessException e) {
+                    // Should never happen
+                    throw new InternalError();
+                }
+            }
+        }
+    }
+
+    public UnicodeBlockSerializer() {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void write(final Kryo kryo, final Output output, final UnicodeBlock obj) {
+        output.writeAscii(BLOCK_NAMES.get(obj));
+    }
+
+    /**
+     * Returns {@code original}; see {@link com.esotericsoftware.kryo.serialize.EnumSerializer#copy}
+     * for why we behave this way.
+     */
+    @Override
+    public UnicodeBlock copy(final Kryo kryo, final UnicodeBlock original) {
+        return original;
+    }
+
+    @Override
+    public UnicodeBlock read(final Kryo kryo, final Input input,
+                             final Class<UnicodeBlock> unicodeBlockClass) {
+        String name = input.readString();
+        return (name == null) ? null : UnicodeBlock.forName(name);
+    }
+}
---
+++ src/test/java/de/javakaffee/kryoserializers/UnicodeBlockSerializerTest.java
@@ -0,0 +1,63 @@
+package de.javakaffee.kryoserializers;
+
+import static de.javakaffee.kryoserializers.KryoTest.deserialize;
+import static de.javakaffee.kryoserializers.KryoTest.serialize;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertSame;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Kryo.DefaultInstantiatorStrategy;
+
+import org.objenesis.ObjenesisStd;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.lang.Character.UnicodeBlock;
+
+/**
+ * Test for {@link UnicodeBlockSerializer}.
+ *
+ * @author <a href="mailto:seahen123@gmail.com">Chris Hennick</a>
+ */
+public class UnicodeBlockSerializerTest {
+
+    private static final String NONEXISTENT_BLOCK_NAME = "RURITANIAN";
+    private Kryo kryo;
+
+    private static class ThingWithUnicodeBlock {
+        final UnicodeBlock unicodeBlock;
+
+        private ThingWithUnicodeBlock(UnicodeBlock unicodeBlock) {
+            this.unicodeBlock = unicodeBlock;
+        }
+    }
+
+    @BeforeTest
+    protected void beforeTest() {
+        kryo = new Kryo();
+        final DefaultInstantiatorStrategy instantiatorStrategy = new DefaultInstantiatorStrategy();
+        instantiatorStrategy.setFallbackInstantiatorStrategy(new StdInstantiatorStrategy());
+        kryo.setInstantiatorStrategy(instantiatorStrategy);
+        kryo.register(UnicodeBlock.class, new UnicodeBlockSerializer());
+    }
+
+    @Test
+    public void testBasicRoundTrip() {
+        byte[] serialized = serialize(kryo, UnicodeBlock.UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS);
+        assertSame(deserialize(kryo, serialized, UnicodeBlock.class),
+                UnicodeBlock.UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS);
+    }
+
+    @Test
+    public void testDeserializingUnknownInstanceReturnsNull() {
+        byte[] serialized = serialize(kryo, new ObjenesisStd().newInstance(UnicodeBlock.class));
+        assertNull(deserialize(kryo, serialized, UnicodeBlock.class));
+    }
+
+    @Test
+    public void testCopyContainingObject() {
+        ThingWithUnicodeBlock original = new ThingWithUnicodeBlock(UnicodeBlock.GREEK);
+        assertSame(kryo.copy(original).unicodeBlock, UnicodeBlock.GREEK);
+    }
+}
