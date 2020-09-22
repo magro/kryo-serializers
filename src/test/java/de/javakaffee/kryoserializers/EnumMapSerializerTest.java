d0c74d066c4ee49d56db7446a7bde4ad49b36433
Mr14huashao <mr11huashao@gamil.com>
2020-09-22 16:26:05 +0800

    Add a test case for issue #90

--- src/test/java/de/javakaffee/kryoserializers/EnumMapSerializerTest.java
+++ src/test/java/de/javakaffee/kryoserializers/EnumMapSerializerTest.java
@@ -2,10 +2,16 @@ package de.javakaffee.kryoserializers;
 
 import static org.testng.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.junit.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -25,12 +31,14 @@ public class EnumMapSerializerTest {
 
     private Kryo _kryo;
     private EnumMap<Vipers, Set<String>> _original;
-    
+    private EnumMap<Vipers, Object> _newTempMap;
+
     @BeforeTest
     protected void beforeTest() {
         _kryo = new Kryo();
 		_kryo.register(EnumMap.class, new EnumMapSerializer());
 		_original = new EnumMap<Vipers, Set<String>>(Vipers.class);
+		_newTempMap = new EnumMap<Vipers, Object>(Vipers.class);
     }
     
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -57,4 +65,35 @@ public class EnumMapSerializerTest {
         assertNotSame(_original.get(Vipers.BLACK_MAMBA), copy.get(Vipers.BLACK_MAMBA));
         assertEquals(_original, copy);
     }
+
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	@Test
+	public void testEnumMapNest() throws Exception {
+		_kryo.register(HashSet.class);
+		_kryo.register(Vipers.class);
+		_kryo.register(Colors.class);
+		_kryo.register(EnumMap.class, new EnumMapSerializer());
+
+		final Set<String> mambaAka = new HashSet<String>();
+		final Set<String> papaAka = new HashSet<>();
+		mambaAka.add("Beatrix Kiddo");
+		mambaAka.add("The Bride");
+		papaAka.add("Good Job");
+		papaAka.add("There is Great");
+
+		_original.put(Vipers.BLACK_MAMBA, mambaAka);
+		_original.put(Vipers.SIDEWINDER, papaAka);
+		_newTempMap.put(Vipers.COPPERHEAD, _original);
+
+		final File outputFile = File.createTempFile("input_file", "dat");
+		try (final Output output = new Output(new FileOutputStream(outputFile))) {
+			_kryo.writeObject(output, _newTempMap);
+			output.flush();
+			output.close();
+			final Input input = new Input(new FileInputStream(outputFile));
+			final EnumMap tEnumMap = _kryo.readObject(input, EnumMap.class);
+			input.close();
+			Assert.assertEquals(_newTempMap, tEnumMap);
+		}
+	}
 }
